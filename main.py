a43085f949dc2bd1f6cacc5b602f45cea1b98911
Neeraj Agarwal <neerajagarwal090@gmail.com>
2020-09-14 19:08:17 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,70 @@
+
+
+import turtle
+
+a=100
+#  print(a)
+# b="tester"
+# print(b)
+# credit_card = 12354125451561515
+
+neeraj_turtle = turtle.Turtle()
+neeraj_turtle.speed(100)
+
+
+
+def squre():
+  neeraj_turtle.forward(a)
+  neeraj_turtle.right(90)
+  neeraj_turtle.forward(a)
+  neeraj_turtle.right(90)
+  neeraj_turtle.forward(a)
+  neeraj_turtle.right(90)
+  neeraj_turtle.forward(a)
+  neeraj_turtle.right(40)
+
+
+human_weight = 20000
+elephent_weight = 5000
+
+
+# if human_weight>elephent_weight:
+#   squre()
+# else:
+#   neeraj_turtle.forward(a)
+
+# count=0
+# neeraj = "active"
+# while neeraj == "active":
+#   neeraj_turtle.forward(10)
+#   if count >10:
+#     neeraj="tired"
+#   count=count+1
+
+#  for count in range(100):
+#   print(count)
+#   squre()
+#   if count>50:
+#     a=200
+#   # neeraj_turtle.forward(20)
+
+lista = [1, 2, 3, 4, 5]
+listb= ["admin", "user", "supervisor", "superadmin"]
+listmix =[1, "admin", True, 2, 4.3, "user"]
+
+# print(lista)
+# print(listb[3])
+# print(listb[2])
+# print(listmix[2])
+
+number = [2,0,5,8,12,1]
+number.sort()
+print(number)
+
+# for num in number:
+#   print(num)
+#   if num>3:
+#     squre()
+
+# print(number[-len(number)])
+print(number[:3])
